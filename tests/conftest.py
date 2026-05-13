8cdff3d9a7988fe1be71f4efa473b2d8a3eddd26
Александр Смирнов <99762067+zzR276q@users.noreply.github.com>
2026-05-13 03:36:22 +0300

    Create conftest.py

---
+++ tests/conftest.py
@@ -0,0 +1,11 @@
+"""
+Конфигурация pytest для CAS
+Автоматически добавляет корневую директорию проекта в sys.path
+"""
+import sys
+import os
+
+# Добавляем корневую директорию проекта в путь поиска модулей
+ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if ROOT_DIR not in sys.path:
+    sys.path.insert(0, ROOT_DIR)
